885a7dcf16e61a2e19569eb16b9c299d3b9e97df
Hermes <hermesko@buffalo.edu>
2016-09-23 19:20:24 -0400

    Added ability to get the last location of the device, currently only displays it once in a textview on the first tab.

--- app/src/main/java/com/example/kevin/wear_where/MainActivity.java
+++ app/src/main/java/com/example/kevin/wear_where/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.kevin.wear_where;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.kevin.wear_where.data.Channel;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,8 +34,10 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, ConnectionCallbacks, OnConnectionFailedListener {
 
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
     TextView temperature, location, description;            // TextView in xml.
     URL request;                                            // The link requesting service from Yahoo query
     Channel channel;                                        // Channel Object
@@ -34,45 +47,89 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set up the tab host
         TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
         tabHost.setup();
 
+        //Create tab1
         TabHost.TabSpec homeTab = tabHost.newTabSpec("homeTab");
         homeTab.setIndicator("Weather");
         homeTab.setContent(R.id.layout1);
         tabHost.addTab(homeTab);
 
+        //Create tab2
         homeTab = tabHost.newTabSpec("homeTab2");
         homeTab.setIndicator("Apparel");
         homeTab.setContent(R.id.layout2);
         tabHost.addTab(homeTab);
 
+        //Create tab3
         homeTab = tabHost.newTabSpec("homeTab3");
         homeTab.setIndicator("Vacation?");
         homeTab.setContent(R.id.layout3);
         tabHost.addTab(homeTab);
 
-        homeTab = tabHost.newTabSpec("homeTab2");
+        //Create tab4
+        homeTab = tabHost.newTabSpec("homeTab4");
         homeTab.setIndicator("Road\nTrip!");
         homeTab.setContent(R.id.layout4);
         tabHost.addTab(homeTab);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
         temperature = (TextView) findViewById(R.id.temperature);
         location = (TextView) findViewById(R.id.location);
         description = (TextView) findViewById(R.id.description);
+        TextView currentLocation = (TextView) findViewById(R.id.locationCoordinates);
 
         channel = new Channel();
         getRequest();
+
+        //Set up google maps fragment in tab4
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //Build Google API Client
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        }
+
+        //Attempt to connect
+        if(mGoogleApiClient!= null){
+            mGoogleApiClient.connect();
+        }
+        else
+            currentLocation.setText("Could not connect to Google Play Services!");
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        int MY_PERMISSION_ACCESS_COARSE_LOCATION = 77;
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(this, new String[] {
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    }, MY_PERMISSION_ACCESS_COARSE_LOCATION );
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            TextView currentLocation = (TextView) findViewById(R.id.locationCoordinates);
+            currentLocation.setText("Current Location: Latitude = " + String.valueOf(mLastLocation.getLatitude()) + ", Longitude = " + String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        TextView currentLocation = (TextView) findViewById(R.id.locationCoordinates);
+        currentLocation.setText("Connection Suspended!");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult arg0) {
+        TextView currentLocation = (TextView) findViewById(R.id.locationCoordinates);
+        currentLocation.setText("Connection Failed!");
     }
 
     private void displayResults() {
