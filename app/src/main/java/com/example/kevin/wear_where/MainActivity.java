650a1c33c9ae00481a52c3e8937dc273c8c0ae29
Kevin <kevinjia@buffalo.edu>
2016-10-21 18:05:57 -0400

    created geolookup that returns a city and state

--- app/src/main/java/com/example/kevin/wear_where/MainActivity.java
+++ app/src/main/java/com/example/kevin/wear_where/MainActivity.java
@@ -739,10 +739,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
             try {
-                List<Address> addresses = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+                List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
                 city = addresses.get(0).getLocality();
                 state = addresses.get(0).getAdminArea();
                 location.setText(city + ", " + state);
+
                 getCurrentRequest();
                 getHourlyRequest();
                 getDailyRequest();
