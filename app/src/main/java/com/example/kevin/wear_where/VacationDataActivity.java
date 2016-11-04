571dbd70449ab3209e36b96eab5c28bb54f257da
Kevin <kevinjia@buffalo.edu>
2016-11-04 18:40:42 -0400

    Added clothing suggestion for each time frame of vacation

--- app/src/main/java/com/example/kevin/wear_where/VacationDataActivity.java
+++ app/src/main/java/com/example/kevin/wear_where/VacationDataActivity.java
@@ -1,5 +1,6 @@
 package com.example.kevin.wear_where;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 
 import com.example.kevin.wear_where.AsyncTask.PlannerAST;
 import com.example.kevin.wear_where.WundergroundData.Planner.PlannerObject;
+import com.example.kevin.wear_where.wear.Clothing;
+import com.example.kevin.wear_where.wear.ClothingActivity;
+
+import java.util.ArrayList;
 
 public class VacationDataActivity extends AppCompatActivity {
 
@@ -56,41 +61,13 @@ public class VacationDataActivity extends AppCompatActivity {
         // Assign text to views
         destinationTV.setText(city + ", " + state);
         dateTV.setText(leaveTime + " - " + returnTime);
+
+        GetData();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // Assign the Linear Layout to variable
-        tfList = (LinearLayout) findViewById(R.id.TimeFrameList);
-        // Get the length of the vacation
-        int vacationDuration = CalculateDuration(leaveTime, returnTime);
-        // Get the suitable timeframe length
-        int vacationTimeFrameLength = CalculateTimeFrame(vacationDuration);
-        // Get the number of timeframes given the duration and timeframe length
-        int numofTimeFrames = CalculateNumberofTimeFrames(vacationDuration, vacationTimeFrameLength);
-        // Get an array of the timeframes being looked up
-        final String timeFrames[] = CreateTimeFrames(vacationTimeFrameLength, leaveTime, returnTime, numofTimeFrames);
-
-        for(int i = 1; i < timeFrames.length; i++){
-            // Layout Inflater
-            LayoutInflater li = LayoutInflater.from(this);
-            // Timeframe view to be added to linear layout list
-            final View timeframe = li.inflate(R.layout.timeframe, null);
-            // Update text for date
-            TextView tempTextView = (TextView) timeframe.findViewById(R.id.Date);
-            DisplayDate(timeFrames[i - 1], timeFrames[i], tempTextView);
-
-            new PlannerAST(timeFrames[i - 1], timeFrames[i], city, state){
-                @Override
-                protected void onPostExecute(PlannerObject result){
-                    // Updates condition chances
-                    DisplayResults(result, timeframe);
-                    // Add timeframe view to
-                    tfList.addView(timeframe);
-                }
-            }.execute();
-        }
 
         // TODO: Do validation in MainActivity that'll determine if this activity getes loaded
 //        if (city == null || state == null) {
@@ -102,20 +79,45 @@ public class VacationDataActivity extends AppCompatActivity {
 //        }
     }
 
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
     // Calculates the duration of stay roughly estimating all months to be 30days
     public int CalculateDuration(String leaving, String returning) {
         int tempLeaveDay = GetDay(leaving);
         int tempReturnDay = GetDay(returning);
         int tempLeaveMonth = GetMonth(leaving);
         int tempReturnMonth = GetMonth(returning);
+        int tempLeaveYear = GetYear(leaving);
+        int tempReturnYear = GetYear(returning);
 
         int tempDuration = 0;
-        // Calculate all days of the vacation months
-        if (tempReturnMonth > tempLeaveMonth) {
-            // Duration of at least a month
-            while (tempLeaveMonth < tempReturnMonth) {
-                tempDuration += DaysInMonth(tempLeaveMonth);
-                tempLeaveMonth++;
+        while(tempLeaveMonth != tempReturnMonth || tempLeaveYear != tempReturnYear) {
+            // Calculate all days of the vacation months where vacation months reside in the same year
+            if (tempReturnMonth > tempLeaveMonth) {
+                // Duration of at least a month
+                while (tempReturnMonth > tempLeaveMonth) {
+                    tempDuration += DaysInMonth(tempLeaveMonth);
+                    tempLeaveMonth++;
+                }
+            }
+            // Calculate all days of the vacation months where vacation extends to a new year
+            else if (tempReturnMonth <= tempLeaveMonth && tempReturnYear > tempLeaveYear) {
+                if (tempLeaveMonth < 12) {
+                    tempDuration += DaysInMonth(tempLeaveMonth);
+                    tempLeaveMonth++;
+                } else if (tempLeaveMonth == 12) {
+                    tempDuration += DaysInMonth(tempLeaveMonth);
+                    tempLeaveMonth = 1;
+                    tempLeaveYear++;
+                }
             }
         }
         // Subtract the days before that you have not left yet from the first month and add the remaining days of the return month
@@ -133,6 +135,12 @@ public class VacationDataActivity extends AppCompatActivity {
     // Returns the month of a given string
     public int GetMonth(String date) {
         String temp = "" + date.charAt(0) + date.charAt(1);
+        return Integer.parseInt(temp);
+    }
+
+    // Returns the year of a given string
+    public int GetYear(String date) {
+        String temp = "" + date.charAt(6) + date.charAt(7) + date.charAt(8) + date.charAt(9);
         return Integer.parseInt(temp);
     }
 
@@ -160,18 +168,20 @@ public class VacationDataActivity extends AppCompatActivity {
     public String[] CreateTimeFrames(int timeFrameLength, String start, String end, int frames) {
         int tempStartMonth = GetMonth(start);
         int tempStartDay = GetDay(start);
+        int tempStartYear = GetYear(start);
         int tempEndMonth = GetMonth(end);
         int tempEndDay = GetDay(end);
+        int tempEndYear = GetYear(end);
         String tempTimeFrames[] = new String[frames + 1];
 
         // Add first beginning timeframe to array
-        tempTimeFrames[0] = FormatDate(tempStartMonth, tempStartDay);
+        tempTimeFrames[0] = FormatDate(tempStartMonth, tempStartDay, tempStartYear);
 
         // Initialize index for after first timeframe
         int index = 1;
 
         // Add every other beginning timeframe to array
-        while (tempStartMonth <= tempEndMonth && tempStartDay != tempEndDay) {
+        while (tempStartMonth != tempEndMonth || tempStartDay != tempEndDay || tempStartYear != tempEndYear) {
             // Increment date to next timeframe
             tempStartDay += timeFrameLength;
 
@@ -179,14 +189,19 @@ public class VacationDataActivity extends AppCompatActivity {
                 // If day exceeds number of days in month then move to the next month
                 tempStartDay -= DaysInMonth(tempStartMonth);
                 tempStartMonth++;
+
+                if(tempStartMonth > 12){
+                    tempStartMonth -= 12;
+                    tempStartYear++;
+                }
             }
 
-            if(tempStartMonth == tempEndMonth && tempStartDay > tempEndDay){
+            if(tempStartMonth == tempEndMonth && tempStartYear == tempStartYear && tempStartDay > tempEndDay){
                 tempStartDay = tempEndDay;
             }
 
             // Add timeframe to index and increment index
-            tempTimeFrames[index] = FormatDate(tempStartMonth, tempStartDay);
+            tempTimeFrames[index] = FormatDate(tempStartMonth, tempStartDay, tempStartYear);
             index++;
         }
 
@@ -205,7 +220,7 @@ public class VacationDataActivity extends AppCompatActivity {
         return temp;
     }
 
-    public String FormatDate(int month, int day) {
+    public String FormatAPIDate(int month, int day) {
         String tempDate;
         if (month < 10) {
             tempDate = "0" + Integer.toString(month);
@@ -214,19 +229,83 @@ public class VacationDataActivity extends AppCompatActivity {
         if (day < 10) {
             tempDate = tempDate + "0" + Integer.toString(day);
         } else tempDate = tempDate + Integer.toString(day);
+
         return tempDate;
+    }
+
+    public String FormatDate(int month, int day, int year) {
+        String tempDate;
+        if (month < 10) {
+            tempDate = "0" + Integer.toString(month);
+        } else tempDate = Integer.toString(month);
+
+        tempDate = tempDate + "/";
+
+        if (day < 10) {
+            tempDate = tempDate + "0" + Integer.toString(day);
+        } else tempDate = tempDate + Integer.toString(day);
+
+        tempDate = tempDate + "/" + Integer.toString(year);
+
+        return tempDate;
+    }
+
+    // Setup Onclicklistener for the clothing suggestion button for each time frame
+    public void SetSuggestionButton(final PlannerObject plannerObject, View timeframe){
+        Button suggestion = (Button) timeframe.findViewById(R.id.Suggestion);
+        suggestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Clothing clothesObject = new Clothing();
+                Intent clothingActivity = new Intent(VacationDataActivity.this, ClothingActivity.class);
+                // Headwear list
+                if(plannerObject.getBelowFreezingChance() >= 30) {
+                    // Get Hearwear for temperature under 32 if the chance of it occuring is above 30%
+                    ArrayList<String> headwear = clothesObject.getHeadWear("31");
+                    String[] headwearList = new String[headwear.size()];
+                    for (int i = 0; i < headwear.size(); i++) {
+                        headwearList[i] = headwear.get(i);
+                    }
+                    clothingActivity.putExtra("Headwear", headwearList);
+                }
+                // Upperbody list
+                ArrayList<String> upperbody = clothesObject.getUpperBody("55");
+                String[] upperbodyList = new String[upperbody.size()];
+                for (int i = 0; i < upperbody.size(); i++){
+                    upperbodyList[i] = upperbody.get(i);
+                }
+                clothingActivity.putExtra("Upperbody", upperbodyList);
+                // Lowerbody list
+                ArrayList<String> lowerbody = clothesObject.getLowerBody("55");
+                String[] lowerbodyList = new String[lowerbody.size()];
+                for (int i = 0; i < lowerbody.size(); i++){
+                    lowerbodyList[i] = lowerbody.get(i);
+                }
+                clothingActivity.putExtra("Lowerbody", lowerbodyList);
+                // Shoes list
+                ArrayList<String> shoes = clothesObject.getShoes("55");
+                String[] shoesList = new String[shoes.size()];
+                for (int i = 0; i < shoes.size(); i++){
+                    shoesList[i] = shoes.get(i);
+                }
+                clothingActivity.putExtra("Shoes", shoesList);
+
+                startActivity(clothingActivity);
+            }
+        });
     }
 
     // Displays the data of the given timeframe
     public void DisplayResults(PlannerObject plannerObject, View timeframe) {
-        TextView tempText = (TextView) timeframe.findViewById(R.id.BelowFreezingPercent);
+        TextView tempText;
+        tempText = (TextView) timeframe.findViewById(R.id.AboveNinetyPercent);
+        tempText.setText("" + plannerObject.getTempOverNinetyChance() + "%");
+        tempText = (TextView) timeframe.findViewById(R.id.AboveSixtyPercent);
+        tempText.setText("" + plannerObject.getTempOverSixtyChance() + "%");
+        tempText = (TextView) timeframe.findViewById(R.id.AboveFreezingPercent);
+        tempText.setText("" + plannerObject.getFreezingChance() + "%");
+        tempText = (TextView) timeframe.findViewById(R.id.BelowFreezingPercent);
         tempText.setText("" + plannerObject.getBelowFreezingChance() + "%");
-        tempText = (TextView) timeframe.findViewById(R.id.CloudyPercent);
-        tempText.setText("" + plannerObject.getCloudyChance() + "%");
-        tempText = (TextView) timeframe.findViewById(R.id.FogPercent);
-        tempText.setText("" + plannerObject.getFogChance() + "%");
-        tempText = (TextView) timeframe.findViewById(R.id.SnowPercent);
-        tempText.setText("" + plannerObject.getSnowChance() + "%");
         tempText = (TextView) timeframe.findViewById(R.id.RainPercent);
         tempText.setText("" + plannerObject.getRainChance() + "%");
         tempText = (TextView) timeframe.findViewById(R.id.WindyPercent);
@@ -236,6 +315,41 @@ public class VacationDataActivity extends AppCompatActivity {
     }
 
     public void DisplayDate(String start, String end, TextView view){
-        view.setText("" + start.charAt(0) + start.charAt(1) + "/" + start.charAt(2) + start.charAt(3) + " - " + end.charAt(0) + end.charAt(1) + "/" + end.charAt(2) + end.charAt(3));
+        view.setText("" + start + " - " + end);
+    }
+
+    public void GetData(){
+        // Assign the Linear Layout to variable
+        tfList = (LinearLayout) findViewById(R.id.TimeFrameList);
+        // Get the length of the vacation
+        int vacationDuration = CalculateDuration(leaveTime, returnTime);
+        // Get the suitable timeframe length
+        int vacationTimeFrameLength = CalculateTimeFrame(vacationDuration);
+        // Get the number of timeframes given the duration and timeframe length
+        int numofTimeFrames = CalculateNumberofTimeFrames(vacationDuration, vacationTimeFrameLength);
+        // Get an array of the timeframes being looked up
+        final String timeFrames[] = CreateTimeFrames(vacationTimeFrameLength, leaveTime, returnTime, numofTimeFrames);
+
+        for(int i = 1; i < timeFrames.length; i++){
+            // Layout Inflater
+            LayoutInflater li = LayoutInflater.from(this);
+            // Timeframe view to be added to linear layout list
+            final View timeframe = li.inflate(R.layout.timeframe, null);
+            // Update text for date
+            TextView tempTextView = (TextView) timeframe.findViewById(R.id.Date);
+            DisplayDate(timeFrames[i - 1], timeFrames[i], tempTextView);
+
+            new PlannerAST(FormatAPIDate(GetMonth(timeFrames[i - 1]), GetDay(timeFrames[i - 1])), FormatAPIDate(GetMonth(timeFrames[i]), GetDay(timeFrames[i])), city, state){
+                @Override
+                protected void onPostExecute(PlannerObject result){
+                    // Updates condition chances
+                    DisplayResults(result, timeframe);
+                    // Setup clothing suggestion button
+                    SetSuggestionButton(result, timeframe);
+                    // Add timeframe view to
+                    tfList.addView(timeframe);
+                }
+            }.execute();
+        }
     }
 }
--- app/src/main/java/com/example/kevin/wear_where/wear/ClothingActivity.java
+++ app/src/main/java/com/example/kevin/wear_where/wear/ClothingActivity.java
@@ -9,6 +9,7 @@ import android.widget.ListView;
 
 import com.example.kevin.wear_where.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.kevin.wear_where.MainActivity.MESSAGE;
@@ -26,8 +27,51 @@ public class ClothingActivity extends Activity {
 
         ListView list = (ListView)findViewById(R.id.list);
         Intent intent = getIntent();
-        String[] clothingList = intent.getStringArrayExtra(MESSAGE);
-        list.setAdapter(new ArrayAdapter<String>(this, R.layout.mylist, R.id.itemname, clothingList));
+        if(intent.getStringArrayExtra(MESSAGE) != null) {
+            String[] clothingList = intent.getStringArrayExtra(MESSAGE);
+            list.setAdapter(new ArrayAdapter<String>(this, R.layout.mylist, R.id.itemname, clothingList));
+        }
+        else {
+            String[] clothingList = CreateVacationSuggestionList();
+            list.setAdapter(new ArrayAdapter<String>(this, R.layout.mylist, R.id.itemname, clothingList));
+        }
+    }
 
+    public String[] CreateVacationSuggestionList(){
+        ArrayList<String> tempList = new ArrayList<>();
+        String[] list;
+
+        // Add Headwear items to arraylist
+        tempList.add("Headwear");
+        list = getIntent().getStringArrayExtra("Headwear");
+        for(int i = 0; i < list.length; i++){
+            tempList.add(list[i]);
+        }
+        // Add Upperbody items to arraylist
+        tempList.add("Upperbody");
+        list = getIntent().getStringArrayExtra("Upperbody");
+        for(int i = 0; i < list.length; i++){
+            tempList.add(list[i]);
+        }
+        // Add Lowerbody items to arraylist
+        tempList.add("Lowerbody");
+        list = getIntent().getStringArrayExtra("Lowerbody");
+        for(int i = 0; i < list.length; i++){
+            tempList.add(list[i]);
+        }
+        // Add Shoes items to arraylist
+        tempList.add("Shoes");
+        list = getIntent().getStringArrayExtra("Shoes");
+        for(int i = 0; i < list.length; i++){
+            tempList.add(list[i]);
+        }
+
+        // Create the String list to be added to view
+        String[] result = new String[tempList.size()];
+        for(int i = 0; i < tempList.size(); i++){
+            result[i] = tempList.get(i);
+        }
+
+        return result;
     }
 }
