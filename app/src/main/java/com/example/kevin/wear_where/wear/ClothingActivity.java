9e2cbb7cd71dddf10ae2ba650f66e18d42f67aa0
Kevin <kevinjia@buffalo.edu>
2016-11-20 16:19:23 -0500

    clothing to consider bringing added

--- app/src/main/java/com/example/kevin/wear_where/wear/ClothingActivity.java
+++ app/src/main/java/com/example/kevin/wear_where/wear/ClothingActivity.java
@@ -59,8 +59,8 @@ public class ClothingActivity extends Activity {
             laterText.setText("Consider also bringing");
             Spanned[] clothingList = CreateVacationSuggestionList();
             currentList.setAdapter(new ArrayAdapter<>(this, R.layout.mylist, R.id.itemname, clothingList));
-            clothingList = CreateVacationSuggestionList();
-            currentList.setAdapter(new ArrayAdapter<>(this, R.layout.mylist, R.id.itemname, clothingList));
+            clothingList = CreateVacationConsiderationList();
+            laterList.setAdapter(new ArrayAdapter<>(this, R.layout.mylist, R.id.itemname, clothingList));
         }
         Button returnButton = (Button) findViewById(R.id.returnButton);
         returnButton.setOnClickListener(new View.OnClickListener() {
@@ -142,6 +142,114 @@ public class ClothingActivity extends Activity {
         // Add Shoes items to arraylist
         tempList.add(Html.fromHtml("<b>SHOES</b>"));
         bundle = getIntent().getBundleExtra(SHOES + ".Bundle");
+        if(bundle.get(SHOES + ".Hot") != null){
+            tempList.add(Html.fromHtml("<b>Hot</b>"));
+            list = bundle.getStringArray(SHOES + ".Hot");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.get(SHOES + ".Warm") != null){
+            tempList.add(Html.fromHtml("<b>Warm</b>"));
+            list = bundle.getStringArray(SHOES + ".Warm");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.get(SHOES + ".Chilly") != null){
+            tempList.add(Html.fromHtml("<b>Chilly</b>"));
+            list = bundle.getStringArray(SHOES + ".Chilly");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.getStringArray(SHOES + ".Freezing") != null) {
+            tempList.add(Html.fromHtml("<b>Freezing</b>"));
+            list = bundle.getStringArray(SHOES + ".Freezing");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+
+        // Create the String list to be added to view
+        Spanned[] result = new Spanned[tempList.size()];
+        for(int i = 0; i < tempList.size(); i++){
+            result[i] = tempList.get(i);
+        }
+
+        return result;
+    }
+    public Spanned[] CreateVacationConsiderationList(){
+        ArrayList<Spanned> tempList = new ArrayList<>();
+        Bundle bundle;
+        String[] list;
+        // Add Upperbody items to arraylist
+        tempList.add(Html.fromHtml("<b>UPPERBODY</b>"));
+        bundle = getIntent().getBundleExtra(UPPERBODY + ".BundleConsider");
+        if(bundle.get(UPPERBODY + ".Hot") != null){
+            tempList.add(Html.fromHtml("<b>Hot</b>"));
+            list = bundle.getStringArray(UPPERBODY + ".Hot");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.get(UPPERBODY + ".Warm") != null){
+            tempList.add(Html.fromHtml("<b>Warm</b>"));
+            list = bundle.getStringArray(UPPERBODY + ".Warm");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.get(UPPERBODY + ".Chilly") != null){
+            tempList.add(Html.fromHtml("<b>Chilly</b>"));
+            list = bundle.getStringArray(UPPERBODY + ".Chilly");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.getStringArray(UPPERBODY + ".Freezing") != null) {
+            tempList.add(Html.fromHtml("<b>Freezing</b>"));
+            list = bundle.getStringArray(UPPERBODY + ".Freezing");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+
+        // Add Lowerbody items to arraylist
+        tempList.add(Html.fromHtml("<b>LOWERBODY</b>"));
+        bundle = getIntent().getBundleExtra(LOWERBODY + ".BundleConsider");
+        if(bundle.get(LOWERBODY + ".Hot") != null){
+            tempList.add(Html.fromHtml("<b>Hot</b>"));
+            list = bundle.getStringArray(LOWERBODY + ".Hot");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.get(LOWERBODY + ".Warm") != null){
+            tempList.add(Html.fromHtml("<b>Warm</b>"));
+            list = bundle.getStringArray(LOWERBODY + ".Warm");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.get(LOWERBODY + ".Chilly") != null){
+            tempList.add(Html.fromHtml("<b>Chilly</b>"));
+            list = bundle.getStringArray(LOWERBODY + ".Chilly");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+        if(bundle.getStringArray(LOWERBODY + ".Freezing") != null) {
+            tempList.add(Html.fromHtml("<b>Freezing</b>"));
+            list = bundle.getStringArray(LOWERBODY + ".Freezing");
+            for (int i = 0; i < list.length; i++) {
+                tempList.add(Html.fromHtml(list[i]));
+            }
+        }
+
+        // Add Shoes items to arraylist
+        tempList.add(Html.fromHtml("<b>SHOES</b>"));
+        bundle = getIntent().getBundleExtra(SHOES + ".BundleConsider");
         if(bundle.get(SHOES + ".Hot") != null){
             tempList.add(Html.fromHtml("<b>Hot</b>"));
             list = bundle.getStringArray(SHOES + ".Hot");
